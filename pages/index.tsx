1d92a69d7a0f0cdcc8f01c1cd28f9cb67c637dbb
Khuong <lekhuongy@gmail.com>
2020-12-24 00:09:26 -0600

    fixed bug where you can constently add responses and made options disappear

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,87 +1,92 @@
-import Head from 'next/head'
-import Header from '../components/header'
-import Footer from '../components/footer'
-import {useState, useEffect} from 'react'
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
 
 const dummyQuestions = [
   {
-    'question': 'QUESTION Yes',
-    'response': ['yes', 'no', 'maybe', '1st']
+    question: 'QUESTION Yes',
+    response: ['yes', 'no', 'maybe', '1st'],
   },
   {
-    'question': 'QUESTION No',
-    'response': ['yes', 'no', 'maybe', '2nd']
+    question: 'QUESTION No',
+    response: ['yes', 'no', 'maybe', '2nd'],
   },
   {
-    'question': 'QUESTION maybe',
-    'response': ['yes', 'no', 'maybe','3rd']
+    question: 'QUESTION maybe',
+    response: ['yes', 'no', 'maybe', '3rd'],
   },
-]
+];
 
-const Home : React.FC= () => {
-
-  const [qNum, setQNum] = useState<number>(0)
-  const [display, setDisplay] = useState<string[]>([])
-  const [options, setOptions] = useState<string[]>([])
-  const [isResponse, setIsResponse] = useState<boolean>(false)
+const Home: React.FC = () => {
+  const [qNum, setQNum] = useState<number>(0);
+  const [resNum, setResNum] = useState<number>(0);
+  const [display, setDisplay] = useState<string[]>([]);
+  const [options, setOptions] = useState<string[]>([]);
+  const [isResponse, setIsResponse] = useState<boolean>(false);
 
   //The goal is to display one question at a time
-  //also display all the possible responses 
+  //also display all the possible responses
 
-  
-  function addQuestion (){
-    if(qNum < dummyQuestions.length){
-      const currQ = dummyQuestions[qNum].question
-      const currOptions = dummyQuestions[qNum].response
-    
+  function addQuestion() {
+    if (qNum < dummyQuestions.length) {
+      const currQ = dummyQuestions[qNum].question;
+      const currOptions = dummyQuestions[qNum].response;
+
       setIsResponse(false);
-      setDisplay([...display, currQ])
-      setOptions(currOptions)
-      setQNum(qNum + 1)
+      setDisplay([...display, currQ]);
+      setOptions(currOptions);
+      setQNum(qNum + 1);
     }
   }
 
-  function addResponse (evt){
-      const response = evt.target.value
-      setDisplay([...display, response])
-      setIsResponse(true)
+  function addResponse(evt) {
+    if (qNum > resNum) {
+      const response = evt.target.value;
+      setDisplay([...display, response]);
+      setIsResponse(true);
+      setResNum(resNum + 1);
+    }
   }
 
   useEffect(() => {
-    if(isResponse) addQuestion()
-  })
-  
+    if (isResponse) addQuestion();
+  });
+
   return (
     <div>
       <Head>
-         <title>Whats4Dinner</title>
-         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Whats4Dinner</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
-      <Header />
-
-      <div className='container'>
-      <button onClick={addQuestion}>Start</button>
-        <div className='chat'>
+      <div className="container">
+        <button onClick={addQuestion}>Start</button>
+        <div className="chat">
           <div className="chat-title">
             <p>Bot</p>
-              <figure className="avatar"></figure>
+            <figure className="avatar"></figure>
           </div>
 
-          <div className='messages'>
-            {display.map(curr => (<div key={curr}> {curr}</div>))}
+          <div className="messages">
+            {display.map((curr) => (
+              <div key={curr}> {curr}</div>
+            ))}
           </div>
-          <div className='message-options'>
-            {options.map(curr => (<button onClick={addResponse} value={curr} key={curr}>{curr}</button>))}
-          </div>
+
+          {qNum === 0 || qNum > resNum ? (
+            <div className="message-options">
+              {options.map((curr) => (
+                <button onClick={addResponse} value={curr} key={curr}>
+                  {curr}
+                </button>
+              ))}
+            </div>
+          ) : (
+            ''
+          )}
         </div>
       </div>
-       
-      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
-
+export default Home;
